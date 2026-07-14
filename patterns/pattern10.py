953ddbedd00523f9cccdd463bfd1c9f97198a78c
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-07-14 10:32:05 -0600

    fixed logic of pattern 10

--- patterns/pattern10.py
+++ patterns/pattern10.py
@@ -3,21 +3,15 @@ class Solution:
         for i in range(n):
 
             for j in range(i+1):
-                print("*", end="")      # Star
-
-            for j in range(n-(i+1)):
-                print(" ", end="")      # Space
+                print("*", end="")      
 
             print()
 
     def lower_side(self, n):
-        for i in range(n):
+        for i in range(n-1):
 
             for j in range(n-i-1):
-                print("*", end="")      # Star
-            
-            for j in range(n-(i-1)):
-                print(" ", end="")      # Space
+                print("*", end="")     
             
             print()
 
